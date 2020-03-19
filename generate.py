232999166c5c65730ab545a823157553f3678e22
AncaStefaniaa <anca.ilicea@yahoo.com>
2020-03-20 01:07:26 +0200

    solved bugs

--- generate.py
+++ generate.py
@@ -2,13 +2,21 @@
 # run like this:
 #   ./generate.py test/source output
 # the generated `output` should be the same as `test/expected_output`
-
+import json
 import os
 import logging
+import sys
+from pathlib import Path
 import jinja2
+from jinja2 import FileSystemLoader, Environment
+
+
+
+#variabile ce retin calea catre directorul curent, respectiv source pentru a elimina sys.argv
+root = os.path.dirname(os.path.abspath(__file__))
+templates_dir = os.path.join(root, "test\source")
 
 log = logging.getLogger(__name__)
-
 
 def list_files(folder_path):
     for name in os.listdir(folder_path):
@@ -18,7 +26,9 @@ def list_files(folder_path):
         yield os.path.join(folder_path, name)
 
 def read_file(file_path):
-    with open(file_path, 'rb') as f:
+    #aici erau deschise in modul binar fisierele si nu se puteau
+    #face operatiile, am schimbat in 'r'
+    with open(file_path, 'r') as f:
         raw_metadata = ""
         for line in f:
             if line.strip() == '---':
@@ -31,25 +41,33 @@ def read_file(file_path):
 
 def write_output(name, html):
     # TODO should not use sys.argv here, it breaks encapsulation
-    with open(os.path.join(sys.argv[2], name+'.html')) as f:
+    # aici vom deschide fisierele in care sa scriem avand calea declarata
+    # initial, in modul "w" pentru a rescrie datele in limbaj html
+    with open(os.path.join(templates_dir + '/file/' + name), "w") as f:
         f.write(html)
+
 
 def generate_site(folder_path):
     log.info("Generating site from %r", folder_path)
-    jinja_env = jinja2.Environment(loader=FileSystemLoader(folder_path + 'layout'))
+    jinja_env = jinja2.Environment(loader=FileSystemLoader(folder_path + '\layout'))
     for file_path in list_files(folder_path):
         metadata, content = read_file(file_path)
-        template_name = metadata['template']
+        #nu exista template_name in fisierele rst
+        #am adaugat cheia layout
+        template_name = metadata['layout']
         template = jinja_env.get_template(template_name)
-        data = dict(metadata, content=content)
-        html = template(**data)
-        write_output(name, html)
-        log.info("Writing %r with template %r", name, template_name)
+        data = dict(metadata, content = content)
+        #in html am pastrat codul html pe care l-am obtinut
+        #prin aplicarea functiei render pe datelele noastre din
+        #dictionar
+        html = template.render(**data, clear = True)
+        write_output(template_name, html)
+        log.info("Writing %r with template %r", os.name, template_name)
 
 
 def main():
-    generate_site(sys.argv[1])
-
+    #apelare folosind calea definita
+    generate_site(templates_dir)
 
 if __name__ == '__main__':
     logging.basicConfig()
---
+++ test/source/test.rst
@@ -0,0 +1,4 @@
+{"title": "Test", "layout": "home.html"}
+---
+
+Buna sunt un text
